5dce3b7ea0e1e00959fdd1fd84988d0adc780550
Uğur Eren <contact@ugureren.net>
2025-03-13 13:21:15 +0300

    feat: add github button to footer

--- packages/website/src/components/Footer/index.tsx
+++ packages/website/src/components/Footer/index.tsx
@@ -1,21 +1,35 @@
 export const Footer: React.FC = () => {
   return (
-    <footer className="flex flex-col justify-center items-center p-3 text-sm bg-elevated">
-      <span>
-        Released under the{' '}
-        <a href="https://opensource.org/licenses/MIT" target="_blank" rel="noreferrer">
-          MIT License
-        </a>
-        .
-      </span>
+    <footer className="flex flex-row p-3 text-sm bg-elevated">
+      <div className="flex-1" />
 
-      <span>
-        Made with ❤️ by{' '}
-        <a href="https://ugureren.net" target="_blank" rel="noreferrer">
-          Uur Eren
-        </a>
-        .
-      </span>
+      <div className="flex flex-col justify-center items-center">
+        <span>
+          Released under the{' '}
+          <a href="https://opensource.org/licenses/MIT" target="_blank" rel="noreferrer">
+            MIT License
+          </a>
+          .
+        </span>
+
+        <span>
+          Made with ❤️ by{' '}
+          <a href="https://ugureren.net" target="_blank" rel="noreferrer">
+            Uur Eren
+          </a>
+          .
+        </span>
+      </div>
+
+      <div className="flex flex-1 items-center justify-end">
+        <iframe
+          src="https://ghbtns.com/github-btn.html?user=ugur-eren&repo=bloons-td6-save-editor&type=watch&count=false&size=large&v=2"
+          width="170"
+          height="30"
+          title="GitHub"
+          className="outline-0"
+        />
+      </div>
     </footer>
   );
 };
