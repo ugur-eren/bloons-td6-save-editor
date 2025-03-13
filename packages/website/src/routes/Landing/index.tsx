ed37c8ad6014d65dbe0bcfef1f5d1b324b79abb3
Uğur Eren <contact@ugureren.net>
2025-03-13 12:43:06 +0300

    feat: edito & copy & save

--- packages/website/src/routes/Landing/index.tsx
+++ packages/website/src/routes/Landing/index.tsx
@@ -1,12 +1,121 @@
+import Editor from '@monaco-editor/react';
+import {editor} from 'monaco-editor';
+import {decrypt, encrypt} from 'encryption';
+import {useRef, useState} from 'react';
+
 import {DragAndDrop} from '../../components';
+import {ChevronLeft, Copy, Download} from '../../assets/icons';
 
 export const Landing: React.FC = () => {
+  const editorRef = useRef<editor.IStandaloneCodeEditor>(null);
+
+  const [save, setSave] = useState<{
+    name: string;
+    data: string;
+  } | null>(null);
+
+  const onEditorMount = (monacoEditor: editor.IStandaloneCodeEditor) => {
+    editorRef.current = monacoEditor;
+  };
+
   const onDrop = async (files: File[]) => {
     const file = files && 'length' in files && files[0];
     if (!file) return;
 
-    const byteArray = await file.bytes();
+    try {
+      const byteArray = await file.bytes();
+      const decrypted = await decrypt(byteArray, '11');
+
+      setSave({name: file.name, data: JSON.stringify(decrypted, null, 2)});
+    } catch (error) {
+      console.error(error);
+      // eslint-disable-next-line no-alert
+      alert('Failed to open the file');
+    }
   };
+
+  const onCancelClick = () => setSave(null);
+
+  const onCopyClick = async () => {
+    if (!editorRef.current) {
+      // eslint-disable-next-line no-alert
+      alert('Could not copy the JSON');
+      return;
+    }
+
+    if (editorRef.current) {
+      const value = editorRef.current.getValue();
+      await navigator.clipboard.writeText(value);
+    }
+  };
+
+  const onSaveClick = async () => {
+    if (!editorRef.current) {
+      // eslint-disable-next-line no-alert
+      alert('Could not save the file');
+      return;
+    }
+
+    if (editorRef.current) {
+      const value = editorRef.current.getValue();
+      const encrypted = await encrypt(JSON.parse(value), '11');
+
+      const blob = new Blob([encrypted], {type: 'application/octet-stream'});
+      const url = URL.createObjectURL(blob);
+
+      const a = document.createElement('a');
+      a.href = url;
+      a.download = save?.name || 'Profile.Save';
+      a.click();
+    }
+  };
+
+  if (save) {
+    return (
+      <div className="flex flex-col flex-1">
+        <header className="flex flex-row justify-between items-center px-4 py-2 bg-elevated">
+          <button
+            onClick={onCancelClick}
+            className="flex flex-row items-center justify-center cursor-pointer bg-background rounded-md pl-2 pr-3 py-1.5"
+          >
+            <ChevronLeft className="w-7 h-7" />
+
+            <span className="text-xl ml-2">Cancel</span>
+          </button>
+
+          <span className="text-xl">{save.name}</span>
+
+          <div className="flex flex-row items-center justify-center gap-3">
+            <button
+              onClick={onCopyClick}
+              className="flex flex-row items-center justify-center cursor-pointer bg-background rounded-md pl-3 pr-3 py-1.5"
+            >
+              <Copy className="w-7 h-7" />
+
+              <span className="text-xl ml-2">Copy JSON</span>
+            </button>
+
+            <button
+              onClick={onSaveClick}
+              className="flex flex-row items-center justify-center cursor-pointer bg-background rounded-md pl-3 pr-3 py-1.5"
+            >
+              <Download className="w-7 h-7" />
+
+              <span className="text-xl ml-2">Save File</span>
+            </button>
+          </div>
+        </header>
+
+        <Editor
+          language="json"
+          value={save.data}
+          theme="vs-dark"
+          wrapperProps={{style: {display: 'flex', flex: 1}}}
+          onMount={onEditorMount}
+        />
+      </div>
+    );
+  }
 
   return (
     <div className="flex flex-col items-center gap-2 px-2 py-8">
@@ -15,7 +124,7 @@ export const Landing: React.FC = () => {
 
         <span>
           Editing save files is not recommended, use at your own risk. You can lose access to online
-          features.
+          features PERMANENTLY.
         </span>
 
         <span>
